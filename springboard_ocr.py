44bf30f228f33fe6505685e066441f7d2e3664e3
drakerehfeld <drakerehfeld@gmail.com>
2017-07-28 18:20:30 -0700

    allow process individual files

--- springboard_ocr.py
+++ springboard_ocr.py
@@ -5,30 +5,33 @@ import cv2
 from pytesseract_custom.src import pytesseract
 
 class SpringboardOcr():
-    def __init__(self, directory_path):
+    def __init__(self, directory_path=None):
         self.app_count = {}
-        if not os.path.isdir(directory_path):
+        if directory_path and not os.path.isdir(directory_path):
             raise ValueError("Invalid input directory")
-        else:
-            self.directory_path = directory_path
+        self.directory_path = directory_path
 
     def process_all(self):
-        for screenshot in os.listdir(self.directory_path):
-            
-            if screenshot.startswith('.'):
-                continue
+        if self.directory_path:
+            for screenshot in os.listdir(self.directory_path):
+                if screenshot.startswith('.'):
+                    continue
+                screenshot_path = os.path.join(self.directory_path, screenshot)
+                self.process_image_file(screenshot_path)
+        else:
+            raise ValueError("No directory path set")
 
-            screenshot_path = os.path.join(self.directory_path, screenshot)
-            screenshot_img = cv2.imread(screenshot_path)
+    def process_image_file(self, img_path):
+        screenshot_img = cv2.imread(screenshot_path)
+        self.process_image_array(screenshot_img)
 
-            if self._text_is_white(screenshot_img):
-                mask = cv2.inRange(screenshot_img, (240,240,240), (255,255,255))
-            else:
-                mask = cv2.inRange(screenshot_img, (0,0,0), (15,15,15))
-
-            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
-            
-            self.increment_app_name(pytesseract.image_to_string(mask))
+    def process_image_array(self, image_arr):
+        if self._text_is_white(screenshot_img):
+            mask = cv2.inRange(screenshot_img, (240,240,240), (255,255,255))
+        else:
+            mask = cv2.inRange(screenshot_img, (0,0,0), (15,15,15))
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+        self.increment_app_name(pytesseract.image_to_string(mask))
 
     def increment_app_name(self, detected_text):
         all_apps = detected_text.split()
